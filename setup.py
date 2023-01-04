a557759063633520c4a29a9c6c6c0aa711d1dedc
Sven Sager <akira@narux.de>
2023-01-04 19:59:47 +0100

    Switch to setuptools for setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-"""Setupscript fuer RevPiPyLoad."""
+"""Setupscript for RevPiCommander."""
 __author__ = "Sven Sager"
 __copyright__ = "Copyright (C) 2018 Sven Sager"
-__license__ = "LGPLv3"
+__license__ = "GPLv3"
 
-from setuptools import setup
-
+from setuptools import find_namespace_packages, setup
 
 setup(
+    name="revpicommander",
     version="0.9.10rc1",
-    # python_requires="~=3.4",
-    install_requires=["PyQt5", "revpimodio2", "zeroconf"],
 
+    packages=find_namespace_packages("src"),
+    package_dir={'': 'src'},
+    include_package_data=True,
+
+    install_requires=[
+        "PyQt5",
+        "revpimodio2",
+        "zeroconf"
+    ],
     entry_points={
         'console_scripts': [
             'revpicommander = revpicommander.revpicommander:main',
@@ -21,23 +28,19 @@ setup(
         ],
     },
 
-    # Additional meta-data
-    name="revpicommander",
+    url="https://revpimodio.org/revpipyplc/",
+    license="GPLv3",
     author="Sven Sager",
     author_email="akira@narux.de",
     maintainer="Sven Sager",
     maintainer_email="akira@revpimodio.org",
-    url="https://revpimodio.org/revpipyplc/",
     description="GUI for Revolution Pi to upload programs and do IO-Checks",
-    long_description=""
-    "The RevPiCommander is a GUI tool to manage your revolution Pi over the\n"
-    "network. You can search for RevPis in your network, manage the settings\n"
-    "of RevPiPyLoad and do IO checks on your local machine. Developing your\n"
-    "control program is very easy with the developer, upload and debug it\n"
-    "over the network.",
+    long_description="The RevPiCommander is a GUI tool to manage your revolution Pi over the\n"
+                     "network. You can search for RevPis in your network, manage the settings\n"
+                     "of RevPiPyLoad and do IO checks on your local machine. Developing your\n"
+                     "control program is very easy with the developer, upload and debug it\n"
+                     "over the network.",
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: POSIX :: Linux",
     ],
-    license="GPLv3",
 )
